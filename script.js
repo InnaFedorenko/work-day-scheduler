595877b4273279b7bc8ed4283a5934039ea06bce
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-04-18 16:16:38 -0400

    added the current date and 24 hours blocks

--- script.js
+++ script.js
@@ -2,13 +2,17 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  // console.log("Test connected script.js";
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -19,5 +23,10 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  // TODO: Add code to display the current date in the header of the page - Done
+    /*** This code will display the current date in th third line of the header*/
+
+    var currDate = dayjs().format('dddd, MMMM D');
+    currDate+='th';
+    $('#currentDay').text(currDate);
 });
