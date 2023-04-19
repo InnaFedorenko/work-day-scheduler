074c1c04cc3fd63a19b6fe39453825e9c01d7efb
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-04-19 11:22:59 -0400

    schedule finished

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ THEN the current day is displayed at the top of the calendar - 👍
 WHEN I scroll down
 THEN I am presented with timeblocks for standard business hours - 👍
 WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future - 👍
 WHEN I click into a timeblock
 THEN I can enter an event
 WHEN I click the save button for that timeblock
--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // in the html.
 $(function () {
   // console.log("Test connected script.js";
-
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -11,23 +11,20 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
-
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page - Done
-    /*** This code will display the current date in th third line of the header*/
+
+  /** This section describes global variables
+   * currDate - current date value, string;
+   * currDay - current day, string;
+   * currTime - current time - integer;
+   */
     var currDate = dayjs();
     var currDay = currDate.format('dddd, MMMM D')+'th';
-    var currTime = currDate.format('H');
+    var currTime = parseInt(currDate.format('H'));
     // currDate+='th';
     $('#currentDay').text(currDay);
     /**This code validate the currennt hour and mark the hours blocks as following:
@@ -35,23 +32,13 @@ $(function () {
      *  - all hour blocks before the current hour in gray ('past' style);
      *  - all hour blocks after the current hout in green ('future' style);
      */
-    // $('div').addClass('past');
     console.log('currTime - '+currTime);
     for (let i = 0; i <= 23; i++) {
-      if ($(`#${i}`).attr('id') === currTime) {
-        console.log(`The div element with id ${i} is `+  currTime);
-        $(`#${i}`).addClass('present');
-      }else if ($(`#${i}`).attr('id') > currTime) {
-        console.log(`Time after current ${i} is `+  currTime);
-        $(`#${i}`).addClass('future');      
-      }
-      else {
-        console.log(`Time before current ${i} is `+  currTime);
-        $(`#${i}`).addClass('past');
-      };
+      //validation for current time
+      if ($(`#${i}`).attr('id') == currTime) {$(`#${i}`).addClass('present');
+      //validation for future time
+      }else if ($(`#${i}`).attr('id') > currTime) {$(`#${i}`).addClass('future');    
+      //validation for past time  
+      }else {$(`#${i}`).addClass('past');};
     }
-
-    // $('#0').addClass('present');
-    // $('#1').addClass('past');
-    // $('#2').addClass('future');
 });
